b387170835e3e0edc033f6e2b00cd35c00573065
Luthfi Dhiya Ramadhan <83807390+LuthfiDhiyaRamadhan@users.noreply.github.com>
2023-07-16 21:50:19 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,16 @@
+<?php
+//Buat koneksi ke database
+$koneksi = mysqli_connect('localhost', 'root', '', 'db_camping');
 
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "db_camping";
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+if (mysqli_connect_errno()) {
+   echo "Gagal terhubung ke MySQL: " . mysqli_connect_error();
+   exit();
+}
+?>
